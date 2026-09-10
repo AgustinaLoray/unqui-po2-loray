e8864487dc2e18964e0b53bcbaffc2b3c1f68d77
AgustinaLoray <agustinaloray@gmail.com>
2026-09-10 09:47:50 -0300

    super

--- tp3/src/ar/edu/unq/po2/tp3/Multioperador.java
+++ tp3/src/ar/edu/unq/po2/tp3/Multioperador.java
@@ -1,5 +1,32 @@
 package ar.edu.unq.po2.tp3;
 
-public class Multioperador {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Multioperador {
+	private List <Integer> numbers= new ArrayList<Integer>(); 
+	
+	private int suma() {
+		int resultado= 0;
+		for (Integer number: numbers) {
+			resultado += number;
+		}
+		return resultado;
+	}
+	
+	private int resta() {
+		int resultado= 0;
+		for (Integer number: numbers) {
+			resultado -= number;
+		}
+		return resultado;
+	}
+	
+	private int multiplicacion() {
+		int resultado= 0;
+		for (Integer number: numbers) {
+			resultado *= number;
+		}
+		return resultado;
+	}
 }
--- tp3/src/ar/edu/unq/po2/tp3/Point.java
+++ tp3/src/ar/edu/unq/po2/tp3/Point.java
@@ -1,5 +1,12 @@
 package ar.edu.unq.po2.tp3;
 
 public class Point {
-
+	int x;
+	int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
+
